15f2d7e8b794f721e6dc45acd396b5ba27317aff
Joel-Crunk <joel.crunk@outlook.com>
2019-02-28 15:48:00 -0500

    This commit will accept ten numbers and return the  sum

--- ConsoleApp52/Program.cs
+++ ConsoleApp52/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp52
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+               
+            
+            Console.Write("Enter a number: ");
+            int var = int.Parse(Console.ReadLine());
+
+                sum = var + sum;
+            }
+            Console.WriteLine($"The sum of these numbers is {sum}");
+            Console.ReadLine();
         }
     }
 }
